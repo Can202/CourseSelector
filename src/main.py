ef7081ef9a659c4d99c41cca637702f0d69d2454
Can202 <mgoopazo@gmail.com>
2024-06-03 17:07:25 -0400

    split data

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 def main():
-    csv_reader(path_file="data.csv")
+    raw_data = csv_reader(path_file="data.csv")
+    data = plain_text_to_array(data=raw_data)
 
 def csv_reader(*, path_file = ""):
-    
     file = open(path_file, "r", encoding="utf-8")
     data = file.read()
-    print(data)
     file.close()
+    return data
+
+def plain_text_to_array(*, data = ""):
+    data_lines = data.split("\n")
+    data_splitted = []
+    for i in range(len(data_lines)):
+        data_splitted.append(data_lines[i].split(","))
+    return data_splitted
 
 
 if __name__ == "__main__":
