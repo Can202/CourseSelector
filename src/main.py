ff5f9eec1e1947c4b00610fc669c45287b10f114
Can202 <mgoopazo@gmail.com>
2024-06-03 17:53:43 -0400

    detect the quantity of courses

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+# Constants
+ROW_SKIP = 1
+COLUMN_SKIP = 2
 
 def main():
-    data = get_data()
     while True:
         option_selected = menu()
         if option_selected == "0":
@@ -36,7 +38,51 @@ def menu():
     return input("Select: ")
 
 def start():
-    pass
+    data = get_data()
+
+    # Doing the algorithm
+    options = get_valid_options(data)
+
+def get_valid_options(data):
+    # Options
+    courses_quantity = len(data) - ROW_SKIP
+    courses_id = []
+    courses_options = []
+    for temp in range(courses_quantity):
+        i = temp + ROW_SKIP
+        courses_options.append(count_options(data[i]))
+        courses_id.append(i)
+    courses_quantity, [courses_id] = many_sorts(courses_options, [courses_id])
+    print(courses_id)
+    
+    
+
+
+def many_sorts(principal, others):
+    n = len(principal)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if principal[j] > principal[j+1]:
+                principal[j], principal[j+1] = principal[j+1], principal[j]
+                for k in range(len(others)):
+                    others[k][j], others[k][j+1] = others[k][j+1], others[k][j]
+                swapped = True
+        if not swapped:
+            break
+    return [principal, others]
+
+        
+
+def count_options(row_data):
+    count = 0
+    for temp in range(len(row_data) - COLUMN_SKIP):
+        i = temp + COLUMN_SKIP
+        if row_data[i] != "":
+            count += 1
+    return count
+
+
 def create_template():
     pass
 
