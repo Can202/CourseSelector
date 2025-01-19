78b006c00dd3d4481aaa87e99dbffa64e961270c
Can202 <mgoopazo@gmail.com>
2025-01-18 23:39:38 -0300

    add weights #5

--- src/pointsys.py
+++ src/pointsys.py
@@ -1,32 +1,35 @@
 from parsing import *
 
 def point_system(the_calendars):
-    points = [50] * len(the_calendars)
+    points = [100] * len(the_calendars)
 
     data = json_reader(path_file="config.json")
 
     start_time=data["start_time"]
     end_time=data["end_time"]
     days=data["days"]
+    hours_weight=data["weight_in_preferred_hours"]
+    days_weight=data["weight_in_preferred_days"]
     
     free_module_quantity_days = data["free_module"]["quantity_days"]
     free_module_min_hours = data["free_module"]["min_hours"]
     free_module_max_hours = data["free_module"]["max_hours"]
     free_module_next_to = data["free_module"]["next_to"]
+    free_module_weight = data["free_module"]["weight"]
 
-    nrc_quantity= data["nrc_quantity"]
-    nrc_alternatives= data["nrc_alternatives"]
+    nrc_quantity= data["nrc"]["nrc_quantity_weight"]
+    nrc_alternatives= data["nrc"]["nrc_alternatives_weight"]
 
 
     for i in range(len(the_calendars)):
-        points[i] += classes_in_between_hours(the_calendars[i], start_time, end_time)
-        points[i] += classes_in_days(the_calendars[i], days)
-        points[i] += free_modules(the_calendars[i], free_module_quantity_days, free_module_min_hours, free_module_max_hours, free_module_next_to)
+        points[i] += classes_in_between_hours(the_calendars[i], start_time, end_time, hours_weight)
+        points[i] += classes_in_days(the_calendars[i], days, days_weight)
+        points[i] += free_modules(the_calendars[i], free_module_quantity_days, free_module_min_hours, free_module_max_hours, free_module_next_to, free_module_weight)
         points[i] += fn_nrc_quantity(the_calendars[i], nrc_quantity,nrc_alternatives)
 
     return points
 
-def classes_in_between_hours(calendar, a, b):
+def classes_in_between_hours(calendar, a, b, weight):
     points = 0
     for i in range(9):
         for j in range(6):
@@ -38,18 +41,18 @@ def classes_in_between_hours(calendar, a, b):
                 else: 
                     pnt = abs(i-b)
                 points -= pnt
-    return points
+    return points * weight
                 
-def classes_in_days(calendar, days):
+def classes_in_days(calendar, days, weight):
     points = 0
     for i in range(9):
         for j in range(6):
             s = what_is(get_day(j),i,calendar)
             if s != "" and not(get_day(j) in days):
-                points -= 5
-    return points
+                points -= 1
+    return points * weight
 
-def free_modules(calendar, fm_days, fm_min, fm_max, fm_next):
+def free_modules(calendar, fm_days, fm_min, fm_max, fm_next, free_module_weight):
     days = 0
     points = 0
     for j in range(6):
@@ -69,7 +72,7 @@ def free_modules(calendar, fm_days, fm_min, fm_max, fm_next):
     points -= 2 * abs(fm_days-days)
 
             
-    return points
+    return points * free_module_weight
 
 def fn_nrc_quantity(calendar, nrc_quantity,nrc_alternatives):
     ct_nrc_quantity = 0
@@ -78,11 +81,7 @@ def fn_nrc_quantity(calendar, nrc_quantity,nrc_alternatives):
         ct_nrc_quantity += 2 * (calendar["nrc"][i].count("/"))
         ct_nrc_alternatives += calendar["other_nrc"][i].count("/")
     
-    if not nrc_quantity:
-        ct_nrc_quantity = 0
-    if not nrc_alternatives:
-        ct_nrc_alternatives = 0
-    return ct_nrc_quantity + ct_nrc_alternatives
+    return ct_nrc_quantity*nrc_quantity + ct_nrc_alternatives*nrc_alternatives
 
 
 def configure():
@@ -109,25 +108,30 @@ def configure():
     fm_max = int(input("Maximum free modules: "))
     fm_next = input("next to: ")
 
-    nrc_quantity = False
+    nrc_quantity = 0
     if input("Do you want to consider nrc quantity to the point system (y/n)?").lower() == "y":
-        nrc_quantity = True
+        nrc_quantity = 1
 
-    nrc_alternatives = False
+    nrc_alternatives = 0
     if input("Do you want to consider nrc alternatives to the point system (y/n)?").lower() == "y":
-        nrc_alternatives = True
+        nrc_alternatives = 1
 
     data = {
         "start_time": start_time,
         "end_time": end_time,
         "days": days,
+        "weight_in_preferred_hours":4,
+        "weight_in_preferred_days":6,
         "free_module": {
             "quantity_days": fm_days,
             "min_hours":fm_min,
             "max_hours":fm_max,
-            "next_to": fm_next
+            "next_to": fm_next,
+            "weight":2
         },
-        "nrc_quantity": nrc_quantity,
-        "nrc_alternatives":nrc_alternatives
+        "nrc":{
+            "nrc_quantity_weight": nrc_quantity,
+            "nrc_alternatives_weight":nrc_alternatives
+        }
     }
     json_writer(path_file="config.json", data=data)
