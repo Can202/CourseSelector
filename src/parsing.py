7b51fb71da7c6473861df851b7dd11d5e955a858
Can202 <mgoopazo@gmail.com>
2025-01-19 16:35:45 -0300

    remove dict section functions as they aren't needed anymore

--- src/parsing.py
+++ src/parsing.py
@@ -86,27 +86,6 @@ def get_schedule_number_array(schedule):
 def schedule_number_array_conflict(array1, array2):
     return np.any(array1 & array2)
 
-# ### Look between two courses and check if they have some conflict hour/day (in Dict format). 
-def dict_courses_conflict(*, dict1, dict2):
-
-    conflict_in_days, conflict_in_hours = False, False
-
-    for i in range(dict1["len"]):
-        for j in range(dict2["len"]):
-            conflict_in_days = dicts_have_same(dict1[f"{i}_days"], dict2[f"{j}_days"])
-            conflict_in_hours = dicts_have_same(dict1[f"{i}_hours"], dict2[f"{j}_hours"])
-
-            if conflict_in_days and conflict_in_hours:
-                return True
-    return False
-
-# ### Check if two lists of the dict have something in common. 
-def dicts_have_same(dict1_list, dict2_list):
-    for i in range(len(dict1_list)):
-        if dict1_list[i] in ''.join(map(str, dict2_list)):
-            return True
-    return False
-
 # ###
 def get_data():
     raw_data = csv_reader(path_file="data.csv")
