957e7058c7d8c7044edb89187bf78f8ed6af5749
andreu729 <andresesparzag@gmail.com>
2025-08-28 09:31:02 -0400

    main_data_to_days_and_modules second version

--- src/sat.py
+++ src/sat.py
@@ -65,35 +65,7 @@ def main_data_to_days_and_modules(main_data, bool_sat_model, variables_dict, tot
             print(nrc_data)
 
             modules_used_array = parsing.get_schedule_number_array(nrc_data)
-            #used_vars = []
-
-            #for block in nrc_data:
-            #    if block[0] == "$":
-            #        # Saving nrcs list for the future.
-            #        nrc_this_course.append(block[1:])
-            #    elif "(" in block or ")" in block:
-            #        pass
-            #    else:
-            #        index = block.find("/")
-            #        block_cut = block[index + 1:]
-            #        index = block_cut.find(":")
-            #        num = block_cut[index + 1:]
-            #        days = block_cut[:index]
-            #
-            #        if "-" in days:
-            #            days = days.split("-")
-            #            for day in days:
-            #                day = day_to_numeric(day)
-            #                bool_sat_model.AddImplication(variables_dict[f"{courses_id[i-1]}_SEC_{i}"],
-            #                                              variables_dict[f"{courses_id[i-1]}_D{day}_M{num}"])
-            #                used_vars.append([day, num])
-            #        else:
-            #            day = day_to_numeric(days)
-            #            bool_sat_model.AddImplication(variables_dict[f"{courses_id[i-1]}_SEC_{i}"],
-            #                                          variables_dict[f"{courses_id[i-1]}_D{day}_M{num}"])
-            #            used_vars.append([day, num])
-
-            # Make negative any other DX_MY
+            # Creating constraints
             for j in range(1, total_days + 1):
                 for k in range(1, total_modules + 1):
                     if modules_used_array[i-1, j-1] == 0:
@@ -105,18 +77,3 @@ def main_data_to_days_and_modules(main_data, bool_sat_model, variables_dict, tot
         i += 1
         nrc_by_course.append(nrc_this_course)
     return nrc_by_course, courses_id
-
-
-def day_to_numeric(day):
-    if day == "L":
-        return 1
-    elif day == "M":
-        return 2
-    elif day == "W":
-        return 3
-    elif day == "J":
-        return 4
-    elif day == "V":
-        return 5
-    else:
-        return -1
